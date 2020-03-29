4dc0155a4d52d57924725ee80970523a5be2ab6e
Kevin Green <kevin@ctrlaltdel.world>
2020-03-29 16:24:26 -0400

    rich cart experience

---
+++ web/src/components/cartDrawer.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import cx from 'classnames'
+
+import { Cart } from 'src/components/cart'
+import { useStore, useToggleCart} from 'src/context/siteContext'
+
+import {
+  Close
+} from 'src/components/svgs'
+
+export const CartDrawer = () => {
+  const { cartIsOpen } = useStore()
+  const toggleCart = useToggleCart()
+  const trap = cartIsOpen ? (
+    <div className='cart__drawer-inner'>
+      <div className='ac rel cart__drawer-header'>
+        <h4>Your Cart</h4>
+        <button type='reset' className='p05 abs cart__drawer-close close right top cb' onClick={() => toggleCart()}>
+          <Close className='block' />
+        </button>
+      </div>
+      <Cart />
+    </div>
+  ) : (
+    false
+  )
+  return (
+    <div
+      className={cx('cart__drawer bcw z10 right top x abs', cartIsOpen && 'is-open')}
+      id='container'
+    >
+      {trap}
+    </div>
+  )
+}
--- web/src/layouts/index.tsx
+++ web/src/layouts/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import { Header } from "src/components/header"
-import { Disclaimer } from "src/components/disclaimer"
+import { Header } from 'src/components/header'
+import { Disclaimer } from 'src/components/disclaimer'
+import { CartDrawer } from 'src/components/cartDrawer'
 import { PasswordWrapper } from './password'
 
-import "src/styles/main.css"
+import 'src/styles/main.css'
 
 const Layout = ({ children, siteMetadata }: { children: any }) => {
   return (
     <React.Fragment>
       <Helmet>
-        <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet" />
+        <link href='https://fonts.googleapis.com/css?family=Space+Mono' rel='stylesheet' />
       </Helmet>
       <PasswordWrapper>
         <div>
           <Disclaimer />
           <Header />
+          <CartDrawer />
           {children}
         </div>
       </PasswordWrapper>
