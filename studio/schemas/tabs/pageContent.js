aeaf938fc2861841e346d409e37cda53a8df21d2
Kevin Green <kevin@ctrlaltdel.world>
2020-04-04 13:18:27 -0400

    studio update with breaking changes

--- studio/schemas/tabs/pageContent.js
+++ studio/schemas/tabs/pageContent.js
@@ -3,6 +3,7 @@ import Tabs from 'sanity-plugin-tabs'
 export default {
   name: "pageContent",
   type: "object",
+  title: "Page Content",
   inputComponent: Tabs,
   fieldsets: [
     { name: "main", title: "Main" },
