0723bc17f70022c222380716dbf146333e6d683e
Kevin Green <kevin@ctrlaltdel.world>
2020-07-10 17:01:48 -0400

    watchmode on by default

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -56,7 +56,8 @@ module.exports = {
       options: {
         projectId: process.env.SANITY_PROJECT_ID,
         dataset: process.env.SANITY_DATASET,
-        token: process.env.SANITY_API_TOKEN
+        token: process.env.SANITY_API_TOKEN,
+        watchMode: true
       }
     },
 
