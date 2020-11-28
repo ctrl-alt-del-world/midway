3aec24d2ddc8902de95a7f8ef37e1e9a18407fc2
Kevin Green <kevin@ctrlaltdel.world>
2020-11-28 12:05:06 -0500

    robust cart component, mobile + free shipping progress example

--- web/src/components/cart.tsx
+++ web/src/components/cart.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-import { PageLink } from 'src/components/link'
-import { useCartTotals, useCartItems, useCheckout, useToggleCart } from 'src/context/siteContext'
+import { useCartTotals, useCartItems } from 'src/context/siteContext'
 
 import {
   LineItem
@@ -9,8 +8,6 @@ import {
 
 export const Cart = () => {
   const lineItems = useCartItems()
-  const openCheckout = useCheckout()
-  const toggleCart = useToggleCart()
   const { total } = useCartTotals()
   return (
     <div>
@@ -23,30 +20,10 @@ export const Cart = () => {
           ))}
         </div>
       ) : (
-        <div className='ac'>
+        <div className='tc p3'>
           Cart is Empty
         </div>
       )}
-      <div className='bcw p1'>
-        <div className='x jcb p05'>
-          <h5 className='ac x caps'>Shipping is Free</h5>
-          {lineItems.length > 0 && (
-            <div className='x f jcb aic'>
-              <span className='s24'>Subtotal</span>
-              <span className='s24'>{total}</span>
-            </div>
-          )}
-        </div>
-        {lineItems.length < 1 ? (
-          <div className='f jcc aic rel x'>
-            <PageLink className='s24 m05'  onClick={() => toggleCart()} to='/'>Let's Go Shooooping</PageLink>
-          </div>
-        ): (
-          <button onClick={() => openCheckout()} type='submit' className='button bcblue cw button--h-black x bcb p1'>
-            <span className='s24 block p1'>Checkout</span>
-          </button>
-        )}
-      </div>
     </div>
   )
 }
--- web/src/components/cartDrawer.tsx
+++ web/src/components/cartDrawer.tsx
@@ -1,35 +1,89 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import cx from 'classnames'
 
 import { Cart } from 'src/components/cart'
-import { useStore, useToggleCart} from 'src/context/siteContext'
+import { useStore, useToggleCart, useCartTotals, useCartItems, useCheckout } from 'src/context/siteContext'
 
 import {
   Close
 } from 'src/components/svgs'
 
+/*
+/    Cart Drawer: This module is a bit more robust then other parts of the theme,
+/    this is because carts are relatively difficult to execute, scale to mobile etc,
+/    feel free to keep more of this styling if you use this component, the structure 
+/    a bit confusing to make sure all elements are visible/scrollable on mobile
+*/
+
 export const CartDrawer = () => {
+  const lineItems = useCartItems()
   const { cartIsOpen } = useStore()
+  const { total } = useCartTotals()
   const toggleCart = useToggleCart()
+  const openCheckout = useCheckout()
   const trap = cartIsOpen ? (
-    <div className='cart__drawer-inner'>
-      <div className='ac rel cart__drawer-header'>
-        <h4>Your Cart</h4>
-        <button type='reset' className='p05 abs no-style cart__drawer-close close right top cb' onClick={() => toggleCart()}>
-          <Close className='block' />
-        </button>
+    <React.Fragment>
+      <div className='cart__drawer-inner'>
+        <div className='ac rel cart__drawer-header df aic jcc'>
+          <button type='reset' className='pa button--none bg--transparent cart__drawer-close close left cb' onClick={() => toggleCart()}>
+            <Close className='db m1' />
+          </button>
+          <h4 className='s26 medium'>Cart</h4>
+        </div>
+        <div className='bg--off-white cart__drawer-shipping df jcc aic tc'>
+          <div className='x ac'>
+            <div className='cart__drawer-progress x pr'>
+              <span className='block pa bg--black left y' style={{ 'width': `${((parseFloat(total)).toFixed(2) / 40) * 100}%` }} />
+            </div>
+            {lineItems.length > 0 && (
+              <>
+                {parseInt(total, 0) <= 40 ? (
+                  <div className='s12 p1'>You're <span className=''>${(40 - (parseFloat(total))).toFixed(2)}</span> away from free shipping!</div>
+                ) : (
+                  <div className='s12 p1'>Your order qualifies for <span className='bold'>Free Shipping!</span></div>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+        <Cart />
       </div>
-      <Cart />
-    </div>
+      <div className={cx('bg--white cart__drawer-buttons', parseInt(total, 0) >= 40 && 'free', lineItems.length >= 1 && 'visible')}>
+        <div className='f fw bcw  left ac cart__drawer-bottom pa bottom x jcc aic rel x'>
+          <div className='x p2 jcb'>
+            {lineItems.length > 0 && (
+              <div className='py1 cart__drawer-sub medium'>
+                <div className='x df jcb aic py18 px1 bcw'>
+                  <span className='s18'>Subtotal</span>
+                  <span className='s18 mono'>${total}</span>
+                </div>
+              </div>
+            )}
+          </div>
+          {lineItems.length < 1 ? (
+              <Link className='button button--lg p3 bold s16 dib tc color--white bg--black medium x'  onClick={() => toggleCart()} to='/'>Continue Shopping</Link>
+          ): (
+            <button onClick={openCheckout} type='submit' className='button bn bt p2 medium bg--black color--black s16 x'>
+              <span className='color--white'>Checkout</span>
+            </button>
+          )}
+        </div>
+      </div>
+    </React.Fragment>
   ) : (
     false
   )
   return (
-    <div
-      className={cx('cart__drawer bcw z10 right top x abs', cartIsOpen && 'is-open')}
-      id='container'
-    >
+    <>
+      <div
+        className={cx('cart__drawer bg--white aist z10 right top x pf', cartIsOpen && 'is-open')}
+        id='container'
+      >
       {trap}
-    </div>
+      </div>
+      {/* Handles the overlay to click-close the cart */}
+      <div onClick={() => toggleCart()} className={cx('cart__drawer-bg bg--white z9 left top x y pf', cartIsOpen && 'is-open')} />
+    </>
   )
 }
